92c591605a05217089758819917c85fb2a88bfc8
EleniMagoula <eleni9669@gmail.com>
2020-02-15 10:20:03 +0200

    Add project files.

---
+++ ChocolateTycoon/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChocolateTycoon.Startup))]
+namespace ChocolateTycoon
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
