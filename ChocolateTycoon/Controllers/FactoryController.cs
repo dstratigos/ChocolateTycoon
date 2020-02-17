fd02fd19b29fd74164b79ac0c4e217ce304ebfdb
Dimitris Stratigos <dstratigos@gmail.com>
2020-02-17 15:58:02 +0200

    Added create new Factory option with corresponding view and everything yo

--- ChocolateTycoon/Controllers/FactoryController.cs
+++ ChocolateTycoon/Controllers/FactoryController.cs
@@ -38,10 +38,55 @@ namespace ChocolateTycoon.Controllers
             var factory = db.Factories
                 .Include(f => f.ProductionUnit)
                 .FirstOrDefault(f => f.ID == id);
-            
+
             return PartialView(factory);
         }
 
-        
+        // GET: Factory/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Factory/Create
+        [HttpPost, ActionName("Create")]
+        public ActionResult CreatePost(string name)
+        {
+            var factories = db.Factories;
+            
+            var factory = new Factory
+            {
+                Name = name,
+                Level = 1,
+                ProductionUnit = new ProductionUnit {MaxProductionPerDay=200 }
+            };
+
+            //foreach (var f in factories)
+            //{
+            //    if (f.Name == factory.Name)
+            //    {
+            //        ModelState.AddModelError("Name", "This name already exists!");
+            //    }
+            //}
+
+            if (ModelState.IsValid)
+            {
+                factories.Add(factory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(factory);
+        }
+
+
+
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+        }
+
+
     }
 }
