bdd3feff4295262807b04551e02d26a6f1260f13
Dimitris Stratigos <dstratigos@gmail.com>
2020-02-23 16:59:13 +0200

    Created api for Factory, ProductionUnits and StorageUnits

---
+++ ChocolateTycoon/Controllers/API/FactoriesController.cs
@@ -0,0 +1,34 @@
+﻿using ChocolateTycoon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using ChocolateTycoon.DTOs;
+using AutoMapper;
+using System.Web.Http;
+
+namespace ChocolateTycoon.Controllers.API
+{
+    public class FactoriesController : ApiController
+    {
+        ApplicationDbContext db;
+
+        public FactoriesController()
+        {
+            db = new ApplicationDbContext();
+        }
+
+        // GET: api/factories
+        public IEnumerable<FactoryDTO> GetFactories()
+        {
+            return db.Factories
+            .Include(f => f.ProductionUnit)
+            .Include(f => f.StorageUnit)
+            .ToList()
+            .Select(Mapper.Map<Factory, FactoryDTO>);
+        }
+
+
+    }
+}
---
+++ ChocolateTycoon/DTOs/ProductionUnitDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ChocolateTycoon.DTOs
+{
+    public class ProductionUnitDTO
+    {
+        [Required]
+        public int FactoryID { get; set; }
+        public int MaxProductionPerDay { get; set; }
+
+        public FactoryDTO FactoryDTO { get; set; }
+    }
+}
