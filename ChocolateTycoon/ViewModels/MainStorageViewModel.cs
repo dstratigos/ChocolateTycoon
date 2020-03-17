5c0c998a8033470ad259c1ff9553ce2bc911f0d0
Dimitris Stratigos <dstratigos@hotmail.com>
2020-03-17 15:30:13 +0200

    Commit message

--- ChocolateTycoon/ViewModels/MainStorageViewModel.cs
+++ ChocolateTycoon/ViewModels/MainStorageViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using ChocolateTycoon.Models;
 
@@ -16,9 +17,28 @@ namespace ChocolateTycoon.ViewModels
         public List<Chocolate> Chocolates { get; set; }
         public IDictionary<string, int> availableChocolates = new Dictionary<string, int>();
 
+        public List<string> enumNames = GetChocolateDisplayNames();
+
         public MainStorageViewModel()
         {
             Chocolates = new List<Chocolate>();
+        }
+
+        private static List<string> GetChocolateDisplayNames()
+        {
+            var values = Enum.GetValues(typeof(ChocolateType));
+            var names = new List<string>();
+
+            foreach (var item in values)
+            {
+                names.Add(item.GetType()
+                    .GetMember(item.ToString())
+                    .First()
+                    .GetCustomAttribute<DisplayAttribute>()
+                    .Name);
+            }
+
+            return names;
         }
     }
 }
