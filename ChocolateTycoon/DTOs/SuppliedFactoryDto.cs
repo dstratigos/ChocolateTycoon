3ee86ee81cc13a33f46c71cba814aba54664f27f
Dimitris Stratigos <dstratigos@gmail.com>
2020-03-23 07:31:37 +0200

    Trying to make Supplier Contract button work.

--- ChocolateTycoon/DTOs/SuppliedFactoryDto.cs
+++ ChocolateTycoon/DTOs/SuppliedFactoryDto.cs
@@ -12,6 +12,6 @@ namespace ChocolateTycoon.DTOs
         public int ID { get; set; }
         public string Name { get; set; }
 
-        public StorageUnitDto StorageUnitDTO { get; set; }
+        //public StorageUnitDto StorageUnitDTO { get; set; }
     }
 }
