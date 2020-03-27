0d7b3f9f0080bedd9f75ea1f34d04784658286c2
Dimitris Stratigos <dstratigos@gmail.com>
2020-03-27 18:00:31 +0200

    ShipmentsReceived works. Created migration and updated the Db successfully.

---
+++ ChocolateTycoon/Migrations/202003271551239_AddedShipmentsReceivedToStorageUnit.cs
@@ -0,0 +1,18 @@
+namespace ChocolateTycoon.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedShipmentsReceivedToStorageUnit : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.StorageUnits", "ShipmentsReceived", c => c.Double(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.StorageUnits", "ShipmentsReceived");
+        }
+    }
+}
