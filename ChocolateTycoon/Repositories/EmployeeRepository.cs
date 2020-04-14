a8c8e6cdb6634ef2467dde9766340ece3fc803f2
EleniMagoula <eleni9669@gmail.com>
2020-04-14 18:03:20 +0300

    persistence and repositories folders

---
+++ ChocolateTycoon/Repositories/EmployeeRepository.cs
@@ -0,0 +1,25 @@
+﻿using ChocolateTycoon.Data;
+using ChocolateTycoon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChocolateTycoon.Repositories
+{
+    public class EmployeeRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public EmployeeRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Employee> GetEmployeesBasedOnStore(int storeId)
+        {
+            return _db.Employees
+                .Where(e => e.StoreID == storeId);
+        }
+    }
+}
