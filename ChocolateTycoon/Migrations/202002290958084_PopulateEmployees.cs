361c670ddfe10cdd026c99c87bbc8809038d9ecb
Dimitris Stratigos <dstratigos@gmail.com>
2020-04-15 18:52:37 +0300

    Edited employees migration and added bitcoin icon to employee view.

--- ChocolateTycoon/Migrations/202002290958084_PopulateEmployees.cs
+++ ChocolateTycoon/Migrations/202002290958084_PopulateEmployees.cs
@@ -7,16 +7,16 @@ namespace ChocolateTycoon.Migrations
     {
         public override void Up()
         {
-            Sql("INSERT INTO Employees (LastName, FirstName, Position, Salary, StoreID) VALUES ('Doe', 'John', 1, 1500, 1)");
-            Sql("INSERT INTO Employees (LastName, FirstName, Position, Salary, StoreID) VALUES ('Doe', 'Jane', 2, 900, 1)");
-            Sql("INSERT INTO Employees (LastName, FirstName, Position, Salary, FactoryID) VALUES ('Jack', 'Black', 3, 1000, 1)");
-            Sql("INSERT INTO Employees (LastName, FirstName, Position, Salary, FactoryID) VALUES ('Fox', 'Samantha', 0, 1700, 1)");
+            Sql("INSERT INTO Employees (LastName, FirstName, Position, Salary, StoreID) VALUES ('Doe', 'John', 1, 60, 1)");
+            Sql("INSERT INTO Employees (LastName, FirstName, Position, Salary, StoreID) VALUES ('Doe', 'Jane', 2, 36, 1)");
+            Sql("INSERT INTO Employees (LastName, FirstName, Position, Salary, FactoryID) VALUES ('Jack', 'Black', 3, 40, 1)");
+            Sql("INSERT INTO Employees (LastName, FirstName, Position, Salary, FactoryID) VALUES ('Fox', 'Samantha', 0, 68, 1)");
             Sql("INSERT INTO Employees (LastName, FirstName, Position, Salary) VALUES ('Middleton', 'Pipa', 0, 1700)");
-            Sql("INSERT INTO Employees (LastName, FirstName, Position, Salary, FactoryID) VALUES ('Smith', 'Robert', 3, 1000, 1)");
-            Sql("INSERT INTO Employees (LastName, FirstName, Position, Salary, StoreID) VALUES ('Professor', 'The', 1, 1500, 2)");
-            Sql("INSERT INTO Employees (LastName, FirstName, Position, Salary, FactoryID) VALUES ('Dean', 'James', 0, 1700, 2)");
-            Sql("INSERT INTO Employees (LastName, FirstName, Position, Salary) VALUES ('Knightley', 'Keira', 2, 900)");
-            Sql("INSERT INTO Employees (LastName, FirstName, Position, Salary) VALUES ('Ashimi', 'Maria', 3, 1000)");
+            Sql("INSERT INTO Employees (LastName, FirstName, Position, Salary, FactoryID) VALUES ('Smith', 'Robert', 3, 40, 1)");
+            Sql("INSERT INTO Employees (LastName, FirstName, Position, Salary, StoreID) VALUES ('Professor', 'The', 1, 60, 2)");
+            Sql("INSERT INTO Employees (LastName, FirstName, Position, Salary, FactoryID) VALUES ('Dean', 'James', 0, 68, 2)");
+            Sql("INSERT INTO Employees (LastName, FirstName, Position, Salary) VALUES ('Knightley', 'Keira', 2, 36)");
+            Sql("INSERT INTO Employees (LastName, FirstName, Position, Salary) VALUES ('Ashimi', 'Maria', 3, 40)");
         }
 
         public override void Down()
